a10cac68b53d96c1ebc5054bb8cb14862f2aee36
Nelson Ramirez <xhyskoax@gmail.com>
2022-12-30 19:11:27 -0300

    conociendo los arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let usuarios = ["Carlos", "Pedro", "José", "Felipe"];
+
+console.log(usuarios.length) // 4 -> cantidad de usuarios / largo del array
+
+console.log(usuarios[0]) // output: Carlos
+console.log(usuarios[3]) // output: Felipe
+
+console.log(usuarios.lastIndexOf("José"))// output: 2 -> lastIndexOf retorna el indice del elemento buscado
